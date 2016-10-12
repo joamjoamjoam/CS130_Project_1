f40999536146e0fe09969b4e02b5402382f5ecc3
joamjoamjoam <trentcalllan@yahoo.com>
2016-10-12 14:57:51 -0700

    Initial Commit

---
+++ p1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  p1
+//
+//  Created by Trent Callan on 10/12/16.
+//  Copyright © 2016 Trent Callan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
